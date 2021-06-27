a8e4cf5bee3ea7a53d9af1457c1f47fd5fdc8e7e
Rohan Gore <gorerohan15@gmail.com>
2021-06-27 17:59:51 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/todo-list'));
+app.use(express.static('./dist/solar-app'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/todo-list/'}),
+    res.sendFile('index.html', {root: './dist/solar-app'}),
 );
 
 // Start the app by listening on the default Heroku port
