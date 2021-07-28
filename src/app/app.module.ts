8be93461ce125c8ebb5647e6a8fdbe38bc632098
Rohan Gore <gorerohan15@gmail.com>
2021-07-28 13:33:59 +0530

    updates to deploy to heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,25 @@ import { BannersComponent } from './Mycomponents/banners/banners.component';
 import { AboutComponent } from './Mycomponents/about/about.component';
 import { NavbarComponent } from './Mycomponents/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GooglePayButtonModule } from '@google-pay/button-angular';
 import { ViewproductComponent } from './Mycomponents/viewproduct/viewproduct.component';
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MysheetComponent } from './Mycomponents/mysheet/mysheet.component';
 import { HttpClientModule } from '@angular/common/http'
-import {MatInputModule} from '@angular/material/input';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MoreAboutSolarComponent } from './Mycomponents/more-about-solar/more-about-solar.component';
 import { ShippingAddressComponent } from './Mycomponents/shipping-address/shipping-address.component';
 import { AddtoCartComponent } from './Mycomponents/addto-cart/addto-cart.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatBadgeModule} from '@angular/material/badge';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatBadgeModule } from '@angular/material/badge';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
@@ -54,7 +56,8 @@ import {MatBadgeModule} from '@angular/material/badge';
     ReactiveFormsModule,
     MatSnackBarModule,
     MatSidenavModule,
-    MatBadgeModule
+    MatBadgeModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
